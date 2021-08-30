ecbc833ee5380b13409f079465cc782ded5249ed
Redjardijas <62295839+Redjardijas@users.noreply.github.com>
2021-08-30 17:29:42 +0300

    Update MainActivity.java
    changes in calculation, fixing of mistakes in counting program

--- MyP/app/src/main/java/com/example/myp/MainActivity.java
+++ MyP/app/src/main/java/com/example/myp/MainActivity.java
@@ -13,7 +13,7 @@ import static com.example.myp.R.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn, btn2;
-    Double pech, pril, podr, perepl;
+    Double pech, pril, podr, perepl, v1;
     Integer param;
     EditText edt1, edt2, edt3, edt5, edt6, edt7, edt9;
     CheckBox dCheckBox, rBCheckBox, kCheckBox;
@@ -31,15 +31,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dCheckBox = findViewById(id.checkBox2);
         rBCheckBox = findViewById(id.checkBox);
         kCheckBox = findViewById(id.checkBox3);
-       /* pril = 20.0;
-        podr = 0.008;
-        perepl = 0.013;*/
         edt5 = findViewById(id.ed5);
         edt6 = findViewById(id.ed6);
         edt7 = findViewById(id.ed7);
       btn2 = findViewById(id.button2);
-      /*  img1 = findViewById(id.img2);
-        img2 = findViewById(id.img3);*/
         txt1 = findViewById(id.size);
         txt2 = findViewById(id.textView2);
         txt3 = findViewById(id.edition);
@@ -55,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     perepl = 0.008;
                     pech = 20.0;
                     param = 2;
+                    v1 = 1.0;
                 }
                 if(rBCheckBox.isChecked()) {
                     pril = 0.036;
@@ -62,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     perepl = 0.004;
                     pech = 10.0;
                     param = 4;
+                    v1 = 0.5;
                 }
                 if(kCheckBox.isChecked()) {
                     pril = 0.0;
@@ -69,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     perepl = 0.008;
                     pech = 0.0;
                     param = 4;
+                    v1 = 0.5;
                 }
                 int a = (edt1.getText() == null) ? 0 : Integer.parseInt(edt1.getText().toString());
                 int b = (edt2.getText() == null) ? 0 : Integer.parseInt(edt2.getText().toString());
@@ -77,15 +75,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int e = (edt6.getText() == null) ? 0 : Integer.parseInt(edt6.getText().toString());
                 int f = (edt7.getText() == null) ? 0 : Integer.parseInt(edt7.getText().toString());
                 int s = (edt9.getText() == null) ? 0 : Integer.parseInt(edt9.getText().toString());
-                double g = a * b/s;
+                double g = a * b/v1*s;
                 Double h = g*perepl*f;
                 Double i = pril*f*g;
                 Double j = g*podr;
                 Double k = f*pech;
                 Double l= g + h + i + j + k;
                 txt2.setText(Double.toString(l/param*c/1000*e/100*d/100));
-                /*String str = edt1.getText().toString();
-                    txt2.setText(str);*/
             }
         });
 
@@ -97,8 +93,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             edt6.getText().clear();
             edt7.getText().clear();
             edt9.getText().clear();
-                            /*String str = edt1.getText().toString();
-                              txt2.setText(str);*/
         });
     }
 
